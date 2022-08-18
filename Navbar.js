fed2ec8d8dcdcd5c48f66fa9bd4708a8eb4c0f17
Max <maxweissman@rogers.com>
2022-08-18 12:45:22 -0400

    Added Navbar

---
+++ Navbar.js
@@ -0,0 +1,39 @@
+import * as React from "react";
+import { NavigationContainer } from '@react-navigation/native'
+import { Text } from "react-native";
+import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
+
+import Map from './screens/Map'
+import Profile from './screens/Profile'
+import Posts from './screens/Posts'
+
+const Tab = createBottomTabNavigator();
+
+export default function NavBar(){
+    return (
+        <NavigationContainer>
+            <Tab.Navigator 
+            initialRouteName="Map"
+            screenOptions={({route}) => ({
+                tabBarIcon: ({focused, color, size}) => {
+                    let iconName
+                    let rn = route.name
+                    if (rn === "Map"){
+                        iconName = focused ? 'home' : 'home-outline'
+                    }
+                    else if (rn === "Posts"){
+                        iconName = focused ? 'Posts' : 'Posts-outline'
+                    }
+                    else if (rn === "Profile"){
+                        iconName = focused ? 'Profile' : 'Profile-outline'
+                    }
+                    return <Text>{iconName}</Text>
+                },
+            })}>
+                <Tab.Screen name="Map" component={Map}/>
+                <Tab.Screen name="Profile" component={Profile}/>
+                <Tab.Screen name="Posts" component={Posts}/>
+            </Tab.Navigator>
+        </NavigationContainer>
+    )
+}
---
+++ screens/Map.js
@@ -0,0 +1,74 @@
+import { StatusBar } from "expo-status-bar";
+import * as React from "react";
+import MapView, { Marker } from "react-native-maps";
+import { Dimensions, StyleSheet, Text, View } from "react-native";
+import * as Location from "expo-location";
+
+export default function Map() {
+  const [location, setLocation] = React.useState(null);
+  const [errorMsg, setErrorMsg] = React.useState(null);
+
+  React.useEffect(() => {
+    (async () => {
+      let { status } = await Location.requestForegroundPermissionsAsync();
+      if (status !== "granted") {
+        setErrorMsg("Permission to access location was denied");
+        return;
+      }
+      let location = await Location.getCurrentPositionAsync({});
+      setLocation(location);
+    })();
+  }, []);
+
+  let text = "Waiting...";
+  let lat;
+  let lon;
+
+  if (errorMsg) {
+    text = errorMsg;
+  } else if (location) {
+    console.log(location);
+    lat = location.coords.latitude;
+    lon = location.coords.longitude;
+  }
+
+  return (
+    <>
+      <View style={styles.container}>
+        {!location ? (
+          <Text style={{ textAlign: "center" }}>{text}</Text>
+        ) : (
+          <MapView
+            style={styles.map}
+            showsUserLocation={true}
+            initialRegion={{
+              latitude: lat,
+              longitude: lon,
+              latitudeDelta: 0.0922,
+              longitudeDelta: 0.0421,
+            }}
+          >
+            {/* <Marker
+              title="You are here"
+              coordinate={{ latitude: lat, longitude: lon }}
+            /> */}
+          </MapView>
+        )}
+        <StatusBar style="auto" />
+      </View>
+    </>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
+  },
+  map: {
+    width: Dimensions.get("window").width,
+    height: Dimensions.get("window").height,
+  },
+});
