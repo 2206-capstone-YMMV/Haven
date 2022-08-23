9dab87e7d11f0e5e3695d41d06554ea50dfd1aca
Max <maxweissman@rogers.com>
2022-08-23 12:43:00 -0400

    Can make new conversations?

--- screens/MessagesTab.js
+++ screens/MessagesTab.js
@@ -5,22 +5,20 @@ import { db } from '../firebase'
 import { collection, onSnapshot, query, where } from 'firebase/firestore' 
 
 export default function MessagesTab({ navigation }) {
-    const [conversations, setConversations] = useState({})
+    const [conversations, setConversations] = useState([])
     useEffect(
         () => 
             onSnapshot(query(collection(db, 'Conversations'), where('user', '==', auth.currentUser?.uid)), (snapshot) =>
             setConversations(snapshot.docs.map(convo => {
-                return convo.data()
+                return {id: convo.id, data: convo.data()}
             }))
             )
     ,[])
-    let convos = []
-    if (Array.isArray(conversations)){ //conversations is initially an empty object, would break the map function
-        convos = conversations 
-    }
+
     return (
         <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center'}}>
-            {convos.map((convo,index) => <Text key={index} onPress={() => navigation.navigate('Message', {ConversationId: convo.ConversationId})}>Conversation with {convo.otherUser}</Text>)}
+            <Text onPress={() => navigation.navigate('NewConversation', {conversations })}>New Conversation</Text>
+            {conversations.map((convo,index) => <Text key={index} onPress={() => navigation.navigate('Message', {ConversationId: convo.id})}>Conversation with {convo.data.otherUser}</Text>)}
         </View>
     )
 }
