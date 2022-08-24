2793a5e5e4d703ca1fb61374bf24a34339b2f0b0
Max <maxweissman@rogers.com>
2022-08-24 12:40:23 -0400

    Can start new conversations with any other user. Excessive db calls have been removed

--- screens/MessagesTab.js
+++ screens/MessagesTab.js
@@ -2,23 +2,51 @@ import React, { useEffect, useState } from 'react'
 import { Text, View } from "react-native";
 import { auth } from '../firebase'
 import { db } from '../firebase'
-import { collection, onSnapshot, query, where } from 'firebase/firestore' 
+import { collection, onSnapshot, query, where, getDocs } from 'firebase/firestore' 
 
 export default function MessagesTab({ navigation }) {
     const [conversations, setConversations] = useState([])
+    const [otherUsers, setOtherUsers] = useState([])
+
     useEffect(
         () => 
             onSnapshot(query(collection(db, 'Conversations'), where('user', '==', auth.currentUser?.uid)), (snapshot) =>
             setConversations(snapshot.docs.map(convo => {
-                return {id: convo.id, data: convo.data()}
+                console.log('Grabbing conversations')
+                return convo.data()
             }))
             )
-    ,[])
+        ,[])
+    
+    const conversationIds = conversations.map(convo => convo.conversationId)
+    const otherUsersId = conversations.map(convo => convo.otherUser)
+
+    if (otherUsersId.length === 0){
+        otherUsersId.push('aaaaaaaaaaa')
+    }
+
+    if (conversations.length > 0 && otherUsers.length === 0){ //After the conversations have been grabbed, but dont want this to repeat when page rerenders
+            getDocs(query(collection(db, 'users'), where('uid', 'in', otherUsersId)))
+            .then(users =>
+            setOtherUsers(users.docs.map(user => {
+                console.log('Grabbing Friends')
+                return user.data()
+            })))
+        }
+
+    
+    const names = otherUsersId.map(id => { //lining up names with the order of the conversationIds
+        for (let i = 0; i < otherUsers.length; i++){
+            if (otherUsers[i].uid === id){
+                return otherUsers[i].name
+            }
+        }
+    })
 
     return (
         <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center'}}>
             <Text onPress={() => navigation.navigate('NewConversation', {conversations })}>New Conversation</Text>
-            {conversations.map((convo,index) => <Text key={index} onPress={() => navigation.navigate('Message', {ConversationId: convo.id})}>Conversation with {convo.data.otherUser}</Text>)}
+            {conversationIds.map((conversationId,index) => <Text key={index} onPress={() => navigation.navigate('Message', {conversationId})}>Conversation with {names[index]}</Text>)}
         </View>
     )
 }
