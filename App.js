83e5c685f3a448e37119d9d8859cd6eea0bcf112
unknown <tpham187@yahoo.com>
2022-08-25 11:16:37 -0500

    upload images

--- App.js
+++ App.js
@@ -1,18 +1,17 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
-import { NavigationContainer } from '@react-navigation/native';
-import { createNativeStackNavigator } from '@react-navigation/native-stack';
-import { SafeAreaProvider } from 'react-native-safe-area-context';
-import Home from './screens/Home'
-import LoginScreen from './screens/LoginScreen';
-import SignUpScreen from './screens/SignUpScreen';
-import NewPost from './screens/NewPost';
-import CameraTake from './components/CameraTake';
-import SinglePost from './screens/SinglePost';
-import { Provider } from 'react-redux';
-import { store } from './redux';
+import { StatusBar } from "expo-status-bar";
+import { StyleSheet, Text, View } from "react-native";
+import { NavigationContainer } from "@react-navigation/native";
+import { createNativeStackNavigator } from "@react-navigation/native-stack";
+import { SafeAreaProvider } from "react-native-safe-area-context";
+import Home from "./screens/Home";
+import LoginScreen from "./screens/LoginScreen";
+import SignUpScreen from "./screens/SignUpScreen";
+import NewPost from "./screens/NewPost";
+import CameraTake from "./components/CameraTake";
+import SinglePost from "./screens/SinglePost";
+import { Provider } from "react-redux";
+import { store } from "./redux";
 const Stack = createNativeStackNavigator();
-
 
 export default function App() {
   return (
@@ -20,12 +19,20 @@ export default function App() {
       <NavigationContainer>
         <SafeAreaProvider>
           <Stack.Navigator>
-            <Stack.Screen name="Login" component={LoginScreen}  />
-            <Stack.Screen name="SignUp" component={SignUpScreen} options={{ title: 'Sign Up' }}/>
-            <Stack.Screen name="NewPost" component={NewPost}  />
-            <Stack.Screen name="CameraTake" component={CameraTake}  />
-            <Stack.Screen name="SinglePost" component={SinglePost}  />
-            <Stack.Screen name="Home" component={Home} options={{ headerShown: false }}/>
+            <Stack.Screen name="Login" component={LoginScreen} />
+            <Stack.Screen
+              name="SignUp"
+              component={SignUpScreen}
+              options={{ title: "Sign Up" }}
+            />
+            <Stack.Screen name="NewPost" component={NewPost} />
+            <Stack.Screen name="CameraTake" component={CameraTake} />
+            <Stack.Screen name="SinglePost" component={SinglePost} />
+            <Stack.Screen
+              name="Home"
+              component={Home}
+              options={{ headerShown: false }}
+            />
           </Stack.Navigator>
         </SafeAreaProvider>
       </NavigationContainer>
@@ -36,8 +43,8 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
   },
 });
--- firebase.js
+++ firebase.js
@@ -2,8 +2,9 @@
 import { initializeApp } from "firebase/app";
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
-import { getFirestore } from 'firebase/firestore' 
-import { getAuth } from 'firebase/auth'
+import { getFirestore } from "firebase/firestore";
+import { getAuth } from "firebase/auth";
+import { getStorage } from "firebase/storage";
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
@@ -12,10 +13,12 @@ const firebaseConfig = {
   projectId: "firstfirebase-ccf3a",
   storageBucket: "firstfirebase-ccf3a.appspot.com",
   messagingSenderId: "279021550950",
-  appId: "1:279021550950:web:64993b88a51f621d919ca7"
+  appId: "1:279021550950:web:64993b88a51f621d919ca7",
 };
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
-export const db = getFirestore(app)
-export const auth = getAuth(app)
+
+export const db = getFirestore(app);
+export const auth = getAuth(app);
+export const storage = getStorage(app);
