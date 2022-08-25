1205cf3f3aff8c6bc6028d848878a86f99988db6
Max <maxweissman@rogers.com>
2022-08-25 11:19:48 -0400

    rebased from friends changes

--- screens/Profile.js
+++ screens/Profile.js
@@ -56,13 +56,12 @@ const  Profile = ({ navigation }) => {
     return (
         <View >
             <View>
-                <Text>Hello {profile.name}</Text>
-                <Text>role: {profile.role}</Text>
-                <Text>email: {profile.email}</Text>
-
+                <Text style={styles.input}>Hello {profile.name}</Text>
+                <Text style={styles.input}>role: {profile.role}</Text>
+                <Text style={styles.input}>email: {profile.email}</Text>
                 <Text
                     onPress={handleSignOut}
-                    style={{ fontSize: 26, fontWeight: "bold" }}
+                    style={styles.button}
                 >
                     Sign Out
                 </Text>
@@ -107,5 +106,49 @@ const styles = StyleSheet.create({
       margin: 5,
       borderColor: '#009688',
       backgroundColor: 'white'
-    }
-})
+    },
+    container: {
+      flex: 1,
+      justifyContent: 'center',
+      alignItems: 'center',
+    },
+    inputContainer: {
+      width: '80%'
+    },
+    input: {
+      backgroundColor: 'white',
+      paddingHorizontal: 15,
+      paddingVertical: 10,
+      borderRadius: 10,
+      marginTop: 5,
+    },
+    buttonContainer: {
+      width: '60%',
+      justifyContent: 'center',
+      alignItems: 'center',
+      marginTop: 40,
+    },
+    button: {
+      backgroundColor: '#0782F9',
+      width: '100%',
+      padding: 15,
+      borderRadius: 10,
+      alignItems: 'center',
+    },
+    buttonOutline: {
+      backgroundColor: 'white',
+      marginTop: 5,
+      borderColor: '#0782F9',
+      borderWidth: 2,
+    },
+    buttonText: {
+      color: 'white',
+      fontWeight: '700',
+      fontSize: 16,
+    },
+    buttonOutlineText: {
+      color: '#0782F9',
+      fontWeight: '700',
+      fontSize: 16,
+    },
+  })
