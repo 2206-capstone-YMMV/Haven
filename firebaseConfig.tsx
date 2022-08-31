b169f25e6c7bea0054ebc6fd561d1c86b08b4b55
unknown <tpham187@yahoo.com>
2022-08-31 11:38:14 -0500

    rebase

--- firebaseConfig.tsx
+++ firebaseConfig.tsx
@@ -6,3 +6,13 @@ export default {
   messagingSenderId: "279021550950",
   appId: "1:279021550950:web:64993b88a51f621d919ca7",
 };
+
+// export default {
+//   apiKey: "AIzaSyAK4mNvs6aZVP-5oTimoHKf1V26J5yMPKY",
+//   authDomain: "practice-problem-359619.firebaseapp.com",
+//   projectId: "practice-problem-359619",
+//   storageBucket: "practice-problem-359619.appspot.com",
+//   messagingSenderId: "889394972098",
+//   appId: "1:889394972098:web:0f8078945026c8a6ca425b",
+//   measurementId: "G-1117EQ38CM",
+// };
